51579a19b476c80bf94f893b47cda0fe512c5834
olisolomons <oli.solomons@gmail.com>
2019-08-09 19:15:36 +0100

    Added mqtt and brief version with option full data

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 const express = require('express');
 const expressWs = require('express-ws');
-const {spawn} = require('child_process');
-
+const {
+    spawn
+} = require('child_process');
+const mqtt = require('mqtt');
 
 const PORT = 8080;
+const DEVICE_UUID = process.argv.length > 3 ? process.argv[2] : '+';
 
 app = express();
 expressWs(app);
 
-api=express.Router()
-app.use('/api',api);
+api = express.Router()
+app.use('/api', api);
 
 let clients = new Set();
-api.ws('/json-data',(ws,req)=>{
+api.ws('/json-data', (ws, req) => {
     console.log('connected');
     clients.add(ws);
 
-    ws.on('close',()=>{
+    ws.on('close', () => {
         clients.delete(ws);
     });
 });
 
-let producer = spawn('python',['json-producer.py']);
-producer.stdout.on('data',(data)=>{
+let client = mqtt.connect('mqtt://eu.thethings.network', {
+    username: process.env.MQTT_APP_ID,
+    password: process.env.MQTT_APP_ACCESS_KEY
+})
+client.on('connect', () => {
+    client.subscribe(`+/devices/${DEVICE_UUID}/up`, () => {
 
-    for (let client of clients) {
-        client.send(data.toString());
+    });
+    client.subscribe('+/devices/+/events/activations', () => {
+
+    });
+});
+
+client.on('message', function(topic, message) {
+    // message is Buffer
+    for (let ws of clients) {
+        ws.send(message.toString());
     }
 });
 
 
-producer.on('exit', (code) => {
-  console.log(`Child exited with code ${code}`);
-});
-
 app.use(express.static('static'));
 
-app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- static/script.js
+++ static/script.js
@@ -7,13 +7,56 @@ $(document).ready(() => {
 
     ws.onmessage = (msg) => {
         wrapper_id++;
-        let data = JSON.parse(msg.data)
-        let elem = $(
-            `<div id="wrapper${wrapper_id}"><div id="tree"></div></div>`
-            );
-        wrapper.prepend(elem);
+        let data = JSON.parse(msg.data);
 
-        let tree = jsonTree.create(data, elem[0]);
+        let time = new Date(data.metadata.time);
+        let timeStr = time.toLocaleDateString();
+        timeStr += ` ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+
+        let short = {
+            Time: timeStr,
+            Counter: data.counter,
+            Payload: data.payload_raw
+        };
+        let elem = $(
+            `<div></div>`
+        );
+        let table = $(`<table></table>`);
+        elem.append(table);
+
+        let headers = $(`<tr></tr>`);
+        let table_body = $(`<tr></tr>`);
+        table.append(headers);
+        table.append(table_body);
+
+        for(let header in short){
+            headers.append($(`<td>${header}</td>`));
+            table_body.append($(`<td>${short[header]}</td>`));
+        }
+
+        wrapper.append(elem);
+
+        table.expanded = false;
+
+        table.click(()=>{
+            console.log(table.expanded);
+            if(table.expanded){
+                table.row.remove();
+            }else{
+                table.row = $('<tr></tr>');
+                let wrapper = $('<td colspan="3"></td>');
+
+                table.row.append(wrapper);
+                table.append(table.row);
+
+                jsonTree.create(data, wrapper[0]);
+
+                wrapper.click(e=>{
+                    e.stopPropagation();
+                });
+            }
+            table.expanded = !table.expanded;
+        });
     };
 
 });
